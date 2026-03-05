d218a6c6da1f08f0a61365a5f0cb0587d4318d5b
MrAhmed0702 <amochi670@rku.ac.in>
2026-03-05 10:29:56 +0530

    User Model is Ready

---
+++ Backend/src/modules/users/user.model.js
@@ -0,0 +1,85 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+
+    profilePicture: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS);
+
+  this.password = await bcrypt.hash(this.password, saltRounds);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export default model("User", userSchema);
